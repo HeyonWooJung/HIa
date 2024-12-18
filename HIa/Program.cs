23fdbfaaf9b1b924d09968149651001bd34ab8ec
정현우 <slander.jung@gmail.com>
2024-12-18 11:38:22 +0900

    [Feat] 기능추가
    기능추가

--- HIa/Program.cs
+++ HIa/Program.cs
@@ -13,6 +13,9 @@ namespace HIa
             Console.WriteLine("1");
             Console.WriteLine("2");
             Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
         }
     }
 }
